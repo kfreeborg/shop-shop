245bb23d2bc8f8e76f85447ecc85f40962777a4a
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2021-03-13 11:21:07 -0700

    refactor components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { ApolloProvider } from "@apollo/react-hooks";
-import ApolloClient from "apollo-boost";
-import { StoreProvider } from "./utils/GlobalState";
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
@@ -10,19 +9,20 @@ import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
+import { StoreProvider } from "./utils/GlobalState";
 import OrderHistory from "./pages/OrderHistory";
 
 const client = new ApolloClient({
   request: (operation) => {
-    const token = localStorage.getItem("id_token");
+    const token = localStorage.getItem('id_token')
     operation.setContext({
       headers: {
-        authorization: token ? `Bearer ${token}` : "",
-      },
-    });
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
   },
-  uri: "/graphql",
-});
+  uri: '/graphql',
+})
 
 function App() {
   return (
@@ -43,6 +43,7 @@ function App() {
         </div>
       </Router>
     </ApolloProvider>
+
   );
 }
 
